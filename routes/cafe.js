5830cd4a290afc53ca8e9979d3fee241d513e1c9
Hritik Gupta <36474421+buuzuu@users.noreply.github.com>
2021-04-09 21:39:08 +0100

    Second Commit

--- routes/cafe.js
+++ routes/cafe.js
@@ -21,8 +21,27 @@ router.post('/getNearBy', async(req, res) => {
 })
 
 
+router.post('/getCurrentDayDeliveryTime', async(req, res) => {
+    const data = {
+        businessEmail: req.body.businessEmail
+    }
+    var dayName = req.body.dayName.toLowerCase()
+    axios.post('https://cs615-project.herokuapp.com/business/getBusinessDayAndTime', data)
+        .then((response) => {
+            var w = response.data
+            w.forEach(element => {
+                if (element.dayName === dayName) {
+                    return res.send(element)
+                }
+            });
+            return res.send({ error: 'Day Provided is wrong.' })
+        }).catch((err) => {
+            console.error(err);
+            return res.send({
+                error: err
+            })
+        });
 
-
-
+})
 
 module.exports = router;
